1cf672a231f99621e05bc3c5409fa80ff50e897e
jagan1857 <107793234+jagan1857@users.noreply.github.com>
2022-09-18 19:27:58 +0530

    Create Config.py

---
+++ Config.py
@@ -0,0 +1,10 @@
+from sample_config import Config
+
+class Development(Config):
+  APP_ID = 13115322
+  API_HASH = "f28fbd1367ddda2e6f863c3129323743"
+  TG_BOT_TOKEN = "5478011283:AAF8wL2dZIeh2p7j-ocz5TtydqowKtRQZ90"
+  AUTH_USERS = [
+      "1459910748"
+      # is a string for LEGACY purposes
+  ]
